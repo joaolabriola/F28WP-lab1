64e75b22994e368de903b4bcb7e5785c5b7018eb
joaolabriola <jsb2000@hw.ac.uk>
2023-11-03 10:16:50 +0400

    implemented javascript, fetching json data using AJAX and updating html

--- week4/script.js
+++ week4/script.js
@@ -1,6 +1,49 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://joaolabriola.github.io/F28WP-lab1/week4/cities1.json');
-ourRequest.onload = function () {
-    console.log(ourRequest.responseText);
-};
-ourRequest.send();
+var cityContainer = document.getElementById("city-info");
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://joaolabriola.github.io/F28WP-lab1/week4/cities1.json');
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+        btn.classList.add("hide-me");       // hide button after data is displayed
+    };
+    ourRequest.send();
+
+});
+
+function renderHTML(data) {
+    var htmlString = "";
+
+    // loop through data object, adding data into strings
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br> Where you can enjoy indoor places like: ";
+
+        for (ii = 0; ii < data[i].places.indoor.length; ii++) {
+            // Loop through the indoor places of the current city.
+            if (ii == 0) {
+                htmlString += data[i].places.indoor[ii];
+            } else {
+                htmlString += ", and " + data[i].places.indoor[ii];
+            }
+        }
+
+        htmlString += '. & enjoy outdoor places like: ';
+        // Loop through the outdoor places of the current city.
+        for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].places.outdoor[ii];
+            } else {
+                htmlString += " and " + data[i].places.outdoor[ii];
+            }
+        }
+
+        htmlString += '.</p>';
+        
+    }
+
+    // add string created with data to html document
+    cityContainer.insertAdjacentHTML('beforeend', htmlString);
+}

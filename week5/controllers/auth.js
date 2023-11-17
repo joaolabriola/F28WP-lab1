d9ee883ee70c6429872a55a039e9f2f2711f57c8
joaolabriola <jsb2000@hw.ac.uk>
2023-11-17 13:35:07 +0400

    week5 Node.js Project
    Set up of home page and registration set up, along with backend connection to MySql database set up on localhost

---
+++ week5/controllers/auth.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+exports.register = (req, res) => {
+    console.log(req.body);
+
+    const {name, email, password, passwordConfirm} = req.body;
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+        if(error){
+            console.log(error);
+            return res.status(500).send("Internal Server Error");
+        }
+        if(results.length > 0){
+            return res.render('register', {
+                message: 'This email is already in use'
+            });
+        } else if (password !== passwordConfirm) {
+            return res.render('register', {
+                message: 'Passwords do not match'
+            });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+        db.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword}, (error, results) =>{
+            if(error) {
+                console.log(error);
+                return res.status(500).send("Internal Server Error");
+            } else {
+                console.log(results);
+                return res.render('register', {
+                    message: 'User registered'
+                });
+            }
+        });
+    });
+};
